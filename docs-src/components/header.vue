db6c8fc528609da2bf52b53f173c5ee8b56751be
xrs4433@outllok.com <xrsovell5201314>
2018-03-25 22:41:27 +0800

    完善

--- docs-src/components/header.vue
+++ docs-src/components/header.vue
@@ -3,19 +3,28 @@
     .header__wrapper
       a.logo(href='https://rhysxia.github.io/xl-vision')
         h1 xl-vision
+      .right
+        xl-button-group
+          xl-button(@click='githubClick',icon='social-github')
 </template>
 <script>
-export default {}
+export default {
+  methods: {
+    githubClick() {
+      window.open('https://github.com/RhysXia/xl-vision')
+    }
+  }
+}
 </script>
 <style lang="scss">
 @import '../../src/styles/variables.scss';
 $header-height: 60px;
 .header {
   position: sticky;
-  top:0;
+  top: 0;
   background-color: #fff;
   margin-bottom: 1em;
-  z-index:1;
+  z-index: 1;
   box-shadow: 0 0 0.5em $--color-border;
   .header__wrapper {
     width: 1250px;
@@ -34,6 +43,16 @@ $header-height: 60px;
       padding: 0;
       height: $header-height;
       line-height: $header-height;
+    }
+  }
+  .right {
+    position: relative;
+    float: right;
+    height: $header-height;
+    display: table;
+    > * {
+      display: table-cell;
+      vertical-align: middle;
     }
   }
 }
